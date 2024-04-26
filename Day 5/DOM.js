cb5a2ec92683b64ed904d5c27a989b205eaf1ba7
Shreysh Mane <shreysh.mane12@gmail.com>
2024-04-26 11:00:12 +0530

    Day 5 DOM

---
+++ Day 5/DOM.js
@@ -0,0 +1,40 @@
+
+// console.log(document);
+
+// console.log(document.body);
+
+// console.log(document.body.childNodes);
+// console.log(document.body.children[2].children[0]);
+
+
+
+// Id selector
+// var greetEle= document.getElementById('greetMsg');
+// console.log(greetEle);
+
+
+// Class selector
+// var listEle= document.getElementsByClassName('listItems');
+// console.log(listEle[0]);
+
+// getElementsByTagName
+// var listEle2= document.getElementsByTagName('li');
+// console.log(listEle2);
+
+ // querySelector
+ // Id => #<NameOfID>
+ // Class => .<NameOfClass>
+ // Tag => <TagName>
+
+//  var greetEle2= document.querySelector('#greetMsg');
+//  console.log(greetEle2);
+
+//  var listEle4= document.querySelector('.listItems');
+//  console.log(listEle4);
+
+ // querySelectorAll
+//  var listEle5= document.querySelectorAll('.listItems');
+//  console.log(listEle5);
+
+ var listEle6= document.querySelectorAll('li');
+ console.log(listEle6);
